adffad64fddcf8bd765d11bcfbb2411dfa4c3a19
joel-furrer <joel.furrer426@gmail.com>
2024-06-25 20:29:49 +0200

    added skeleton for moves.cpp and changed translationStage

--- moves.cpp
+++ moves.cpp
@@ -1,6 +1,145 @@
 #include <iostream>
 
-void; rot() {
 
+// Rotations are clockwise by default
+// CC means counterclockwise
+// 2 means its executes twice
+
+
+// ---------------------< F >-------------------------
+int rotF() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotFCC() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotF2() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+// ---------------------< R >-------------------------
+
+int rotR() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotRCC() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotR2() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+// ---------------------< U >-------------------------
+
+int rotU() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotUCC() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotU2() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+// ---------------------< L >-------------------------
+
+int rotL() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotLCC() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotL2() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+
+// ---------------------< B >-------------------------
+
+int rotB() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotBCC() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotB2() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+// ---------------------< D >-------------------------
+
+int rotD() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotDCC() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
+};
+
+int rotD2() {
+
+    //Move the Array according to the given rotation
+
+    return 0; 
 };
 
--- translateStage.cpp
+++ translateStage.cpp
@@ -1,1 +1,5 @@
 #include <iostream>
+
+void translator(filename) {
+    
+}
