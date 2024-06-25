467fc2dff26d877f1b7313b0f668c099bf6ca26b
joel-furrer <joel.furrer426@gmail.com>
2024-06-25 19:51:12 +0200

    Changes

--- translateStage.cpp
+++ translateStage.cpp
@@ -1,2 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+// Function to translate stage file into cube array
+bool translateStage(const string& filename, char cube[6][3][3]) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Unable to open file: " << filename << endl;
+        return false;
+    }
+
+    vector<vector<vector<char>>> cubeVector(6, vector<vector<char>>(3, vector<char>(3)));
+
+    // Read data from file
+    char sideName[20];
+    for (int i = 0; i < 6; ++i) {
+        file.getline(sideName, 20);  // Read side name line
+        for (int j = 0; j < 3; ++j) {
+            file >> cubeVector[i][j][0] >> cubeVector[i][j][1] >> cubeVector[i][j][2];
+        }
+    }
+
+    // Convert cubeVector to cube array
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 3; ++k) {
+                cube[i][j][k] = cubeVector[i][j][k];
+            }
+        }
+    }
+
+    file.close();
+    return true;
+}
+
+int main() {
+    char cube[6][3][3];
+
+
+    return 0;
+}
